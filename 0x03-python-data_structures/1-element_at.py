8d39056a79027060f20ebf3a48c082315c93a7aa
Okikiola <yusuffdayo@gmail.com>
2022-12-05 17:56:02 -0800

    element fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,4 @@
-375aa2:/alx-higher_level_programming/0x03-python-data_structures# emacs 2-replace_in_list.py
-root@ff4dd5375aa2:/alx-higher_level_programming/0x03-python-data_structures# emacs 3-print_reversed_list_integer.py
-root@ff4dd5375aa2:/alx-higher_level_programming/0x03-python-data_structures# emacs 4-new_in_list.py
-root@ff4dd5375aa2:/alx-higher_level_programming/0x03-python-data_structures# emacs
-77;30601;0c#!/usr/bin/python3
+#!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
@@ -13,8 +9,3 @@ def element_at(my_list, idx):
         return (None)
 
     return(my_list[idx])
-
-
-
-
-
