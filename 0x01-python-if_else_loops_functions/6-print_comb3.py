19589dda02a3cd6b924376821a6476930b82b625
Okikiola <yusuffdayo@gmail.com>
2022-12-11 12:33:09 -0800

    update 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for digit1 in range(0, 9):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8:
-            print("{:d}{:d}".format(digit1, digit2))
-            break
-        print("{:d}{:d}".format(digit1, digit2), end=", ")
+for num1 in range(0, 9):
+    for num2 in range(0, 10):
+        if num1 < num2:
+            if num1 < 8:
+                print("{:d}{:d}".format(num1, num2), end=", ")
+            else:
+                print("{:d}{:d}".format(num1, num2))
